372c745e630913a046c32c1c3540d2c2b734b0ac
Tejas Sathe <sathe.tejas@gmail.com>
2015-10-18 12:37:30 -0400

    Fixed template directory problem

--- cost/settings.py
+++ cost/settings.py
@@ -109,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/tejas/Programming/Django/cost/cost/templates",
+    os.path.join(BASE_DIR, 'cost/templates'),
 )
 
 INSTALLED_APPS = (
