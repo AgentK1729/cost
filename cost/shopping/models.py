cbe11f4f7e35b77798792de2adbc8ac35b0f2db6
Tejas Sathe <sathe.tejas@gmail.com>
2015-10-18 11:40:36 -0400

    Added geolocation requirement

--- cost/shopping/models.py
+++ cost/shopping/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
-from django_google_maps import fields as map_fields
 
 class Item(models.Model):
     user = models.ForeignKey(User)
