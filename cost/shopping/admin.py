c97037ddc3a5ca1f60037117b7c7ce7e4d518a7a
Tejas Sathe <sathe.tejas@gmail.com>
2015-10-18 12:32:32 -0400

    Removed google maps error

--- cost/shopping/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django_google_maps import widgets as map_widgets
-from django_google_maps import fields as map_fields
-
-class PickupLocationAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},    
-    }
-
