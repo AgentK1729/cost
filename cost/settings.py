69f2d77b5ca6e19bca6211a6a974fd419a28e761
Tejas Sathe <sathe.tejas@gmail.com>
2015-10-18 12:41:46 -0400

    Fixed static directory problem

--- cost/settings.py
+++ cost/settings.py
@@ -1,4 +1,4 @@
-# Django settings for collegevent project.
+# Django settings for cost project.
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -47,7 +47,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/collegevent/collegevent/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cost/media'),
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -58,7 +58,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/collegevent/collegevent/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'cost/static'),
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
